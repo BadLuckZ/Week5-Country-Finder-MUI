1f5567857f8029a00ee5fcc5df3a7b315d4d89aa
BadLuckZ <thanagorn8802@gmail.com>
2024-10-13 10:45:22 +0700

    Create Router Template

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import CountryDetail from "./routes/CountryDetail.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hi!</div>,
+    element: <App />,
+  },
+  {
+    path: "/countries/:id",
+    element: <CountryDetail />,
   },
 ]);
 
---
+++ src/routes/CountryDetail.jsx
@@ -0,0 +1,3 @@
+export default function CountryDetail() {
+  return <div>Hello!</div>;
+}
